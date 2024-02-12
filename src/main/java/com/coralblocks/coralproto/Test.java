01c645f1fbde9b6686d85fa1073c3d48e4a3193a
Sergio Oliveira <sergio.oliveira@coralblocks.com>
2024-02-12 10:08:24 -0500

    Start of Work

---
+++ src/main/java/com/coralblocks/coralproto/Test.java
@@ -0,0 +1,19 @@
+package com.coralblocks.coralproto;
+
+public class Test {
+
+	public static void main(String[] args) {
+
+		String s = """
+
+		blah blah
+		    here 
+		test
+
+		blah
+
+		""";
+
+		System.out.println(s);
+	}
+}
