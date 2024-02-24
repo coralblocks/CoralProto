dd9bf3e4a388fe04faffd3abaef1a2be960d5b04
Sergio Oliveira <sergio.oliveira@coralblocks.com>
2024-02-24 09:28:12 -0500

    Adding License Copyright

--- src/main/java/com/coralblocks/coralproto/util/IntMap.java
+++ src/main/java/com/coralblocks/coralproto/util/IntMap.java
@@ -1,5 +1,5 @@
 /* 
- * Copyright 2023 (c) CoralBlocks - http://www.coralblocks.com
+ * Copyright 2024 (c) CoralBlocks - http://www.coralblocks.com
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
